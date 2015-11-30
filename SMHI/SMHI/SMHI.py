333a1817d9ee34eefef1e7a8aec226b13db0b6b2
Staffan Hemma <azworle@hotmail.com>
2015-11-30 20:01:32 +0100

    Hämtar svar, skriver objekt, skriver vindhastighet

--- SMHI/SMHI/SMHI.py
+++ SMHI/SMHI/SMHI.py
@@ -1,20 +1,22 @@
-﻿import urllib2
+﻿import urllib.request
 import json
 
 # hämta rapport
-smhiRapport = urllib2.urlopen('http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/57.69/lon/11.96/data.json')
+smhiRapport = urllib.request.urlopen("http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/57.69/lon/11.96/data.json")
 
 #gör om till listobjekt
-j_obj = json.load(smhiRapport)
+str_response = smhiRapport.readall().decode('utf-8')
+j_obj2 = json.loads(str_response)
+
 
 #iterera genom lista, skriv varje rad
 
 counter = 1
-##for i in range(1, len(j_obj['timeseries'])):
-#
-#    if '22:00' in str(j_obj['timeseries'][i]['validTime']):
-#
-#        print str(j_obj['timeseries'][i]['validTime']) + " så blåser det max " + str(j_obj['timeseries'][i]['gust'])
+for i in range(1, len(j_obj2['timeseries'])):
+
+    if '22:00' in str(j_obj2['timeseries'][i]['validTime']):
+
+        print (str(j_obj2['timeseries'][i]['validTime']) + " så blåser det max " + str(j_obj2['timeseries'][i]['gust']))
 
 
 #age = raw_input("hur gammal")
