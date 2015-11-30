6a9738137152dfc91843309d4d482032908fab85
Staffan Hemma <azworle@hotmail.com>
2015-11-30 21:32:02 +0100

    En iteration, hämtar morgondagens temperatur kl 0800

--- SMHI/SMHI/SMHI.py
+++ SMHI/SMHI/SMHI.py
@@ -1,5 +1,6 @@
 ﻿import urllib.request
 import json
+import datetime
 
 # hämta rapport
 smhiRapport = urllib.request.urlopen("http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/57.69/lon/11.96/data.json")
@@ -8,16 +9,25 @@ smhiRapport = urllib.request.urlopen("http://opendata-download-metfcst.smhi.se/a
 str_response = smhiRapport.readall().decode('utf-8')
 j_obj2 = json.loads(str_response)
 
-
 #iterera genom lista, skriv varje rad
+print(j_obj2)
 
-counter = 1
+dag2 = datetime.datetime.now()
+
+dag2 += datetime.timedelta(days=1)
+
+
 for i in range(1, len(j_obj2['timeseries'])):
 
-    if '22:00' in str(j_obj2['timeseries'][i]['validTime']):
+    if '08:00' in str(j_obj2['timeseries'][i]['validTime']):
+        #print(dag2.date())
+        #print(str(j_obj2['timeseries'][i]['validTime']))
 
-        print (str(j_obj2['timeseries'][i]['validTime']) + " så blåser det max " + str(j_obj2['timeseries'][i]['gust']))
+        if str(dag2.date()) in str(j_obj2['timeseries'][i]['validTime']):
+            
+            print("Temp imorgon " + str(j_obj2['timeseries'][i]['t']))
+
+        
 
 
-#age = raw_input("hur gammal")
     
