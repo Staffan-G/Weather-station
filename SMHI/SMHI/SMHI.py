d01517b40c1b30a41d5737d8bf8fb613f512bb48
Staffan Hemma <azworle@hotmail.com>
2015-11-30 22:24:09 +0100

    Sparar morgondagens väder kl 8 och 10 till variabler

--- SMHI/SMHI/SMHI.py
+++ SMHI/SMHI/SMHI.py
@@ -1,6 +1,7 @@
 ﻿import urllib.request
 import json
 import datetime
+import pprint
 
 # hämta rapport
 smhiRapport = urllib.request.urlopen("http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/57.69/lon/11.96/data.json")
@@ -9,23 +10,29 @@ smhiRapport = urllib.request.urlopen("http://opendata-download-metfcst.smhi.se/a
 str_response = smhiRapport.readall().decode('utf-8')
 j_obj2 = json.loads(str_response)
 
-#iterera genom lista, skriv varje rad
-print(j_obj2)
-
+#skapa datumobjekt, dag2 är imorgon
 dag2 = datetime.datetime.now()
-
 dag2 += datetime.timedelta(days=1)
 
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(j_obj2['timeseries'][17])
 
+#iterera genom lista, leta efter datum och tid
 for i in range(1, len(j_obj2['timeseries'])):
 
-    if '08:00' in str(j_obj2['timeseries'][i]['validTime']):
-        #print(dag2.date())
-        #print(str(j_obj2['timeseries'][i]['validTime']))
-
-        if str(dag2.date()) in str(j_obj2['timeseries'][i]['validTime']):
+    if str(dag2.date()) in str(j_obj2['timeseries'][i]['validTime']):
             
-            print("Temp imorgon " + str(j_obj2['timeseries'][i]['t']))
+                if '08:00' in str(j_obj2['timeseries'][i]['validTime']):
+                    dag20800 = str(j_obj2['timeseries'][i]['t'])
+                    print("Temp imorgon 08:00 " + str(j_obj2['timeseries'][i]['t']))
+
+                if '10:00' in str(j_obj2['timeseries'][i]['validTime']):
+                    dag21000 = str(j_obj2['timeseries'][i]['t'])
+                    print("Temp imorgon 10:00 " + str(j_obj2['timeseries'][i]['t']))
+                
+        
+
+       
 
         
 
