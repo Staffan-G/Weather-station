3b37b09abcf2d8d1c4b514ba422221b590010421
Staffan Hemma <azworle@hotmail.com>
2015-11-30 19:31:22 +0100

    Testar

---
+++ SMHI/SMHI/SMHI.py
@@ -0,0 +1,21 @@
+﻿import urllib2
+import json
+
+# hämta rapport
+smhiRapport = urllib2.urlopen('http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/57.69/lon/11.96/data.json')
+
+#gör om till listobjekt
+j_obj = json.load(smhiRapport)
+
+#iterera genom lista, skriv varje rad
+
+counter = 1
+##for i in range(1, len(j_obj['timeseries'])):
+#
+#    if '22:00' in str(j_obj['timeseries'][i]['validTime']):
+#
+#        print str(j_obj['timeseries'][i]['validTime']) + " så blåser det max " + str(j_obj['timeseries'][i]['gust'])
+
+
+#age = raw_input("hur gammal")
+    
