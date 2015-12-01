426caf236f1cb67dbb30261ca7f62815226e172a
Staffan Hemma <azworle@hotmail.com>
2015-12-01 22:17:01 +0100

    Fönster börjar ta form

--- SMHI/SMHI.py
+++ SMHI/SMHI.py
@@ -2,8 +2,10 @@
 import json
 import datetime
 import pprint
-import tkinter
 import io
+import tkinter
+from tkinter import *
+from tkinter import ttk
 
 # hämta rapport
 smhiRapport = urllib.request.urlopen("http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/57.69/lon/11.96/data.json")
@@ -24,6 +26,7 @@ pp.pprint(j_obj2['timeseries'][17])
 
 #gör lista av datum och tid
 tider = ['07:00', '09:00', '11:00', '13:00', '15:00', '18:00']
+
 dag2Temp = []
 dag2Neder = []
 dag2Snö = []
@@ -58,22 +61,49 @@ for i in range(0, len(j_obj2['timeseries'])):
                   dag2Moln.append(str(j_obj2['timeseries'][i]['tcc']))
                   print(str(dag2.date()) + tider[j] + ": " + dag2Temp[j] + " grader. Nederbörd: " + dag2Neder[j] + "mm. Moln: " + dag2Moln[j])
 
-for k in range(0, len(j_obj2['timeseries'])):
-    
-    if str(dag3.date()) in str(j_obj2['timeseries'][k]['validTime']): 
-        
-        for l in range(0, len(tider)):
-            
-            if tider[l] in str(j_obj2['timeseries'][k]['validTime']):
 
-                dag3Temp.append(str(j_obj2['timeseries'][k]['t']))
-                dag3Neder.append(str(j_obj2['timeseries'][k]['pit']))
-                dag3Snö.append(str(j_obj2['timeseries'][k]['pis']))
-                dag3Moln.append(str(j_obj2['timeseries'][k]['tcc']))
+    
+    if str(dag3.date()) in str(j_obj2['timeseries'][i]['validTime']): 
+        
+        for k in range(0, len(tider)):
+            
+            if tider[k] in str(j_obj2['timeseries'][i]['validTime']):
+
+                dag3Temp.append(str(j_obj2['timeseries'][i]['t']))
+                dag3Neder.append(str(j_obj2['timeseries'][i]['pit']))
+                dag3Snö.append(str(j_obj2['timeseries'][i]['pis']))
+                dag3Moln.append(str(j_obj2['timeseries'][i]['tcc']))
                                 
-                print(str(dag3.date()) + tider[l] + ": " + dag3Temp[-1] + " grader. Nederbörd: " + dag3Neder[-1] + "mm. Moln: " + dag3Moln[-1])
+                print(str(dag3.date()) + tider[k] + ": " + dag3Temp[-1] + " grader. Nederbörd: " + dag3Neder[-1] + "mm. Moln: " + dag3Moln[-1])
+
+
+  
 
 #rita upp display
+
+root = Tk()
+content = ttk.Frame(root)
+
+#lägg innehållet här
+
+dag28 = str(dag2.hour()) + ": Regn: " + dag2Neder[0]
+
+ 
+dag2Datum = ttk.Label(content, text=str(dag2.date()))
+pos1 = ttk.Label(content, text=dag28)
+
+
+    
+
+#sätter ut ett grid
+content.grid(column=0, row=0)
+
+#placerar namelb på plats 0,0
+dag2Datum.grid(column=0, row=0)
+pos1.grid(column=0, row=2, sticky=(W))
+
+
+root.mainloop()
 
                     
                 
