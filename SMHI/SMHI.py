779834424b1465639428b13b2bb6269e78a7d39e
Staffan Jobb <azworle@hotmail.com>
2015-12-01 12:42:08 +0100

    Nytt med listor

--- SMHI/SMHI.py
+++ SMHI/SMHI.py
@@ -11,29 +11,29 @@ str_response = smhiRapport.readall().decode('utf-8')
 j_obj2 = json.loads(str_response)
 
 #skapa datumobjekt, dag2 är imorgon
-dag2 = datetime.datetime.now()
-dag2 += datetime.timedelta(days=1)
+dag2 = datetime.datetime.now() + datetime.timedelta(days=1)
+#dag2 += datetime.timedelta(days=1)
 
 #pp = pprint.PrettyPrinter(indent=4)
 #pp.pprint(j_obj2['timeseries'][17])
+
+#gör lista av datum och tid
+list1 = ['08:00', '10:00']
+list2 = []
 
 #iterera genom lista, leta efter datum och tid
 for i in range(1, len(j_obj2['timeseries'])):
 
     if str(dag2.date()) in str(j_obj2['timeseries'][i]['validTime']):
-            
-                if '08:00' in str(j_obj2['timeseries'][i]['validTime']):
-                    dag20800 = str(j_obj2['timeseries'][i]['t'])
-                    print("Temp imorgon 08:00 " + str(j_obj2['timeseries'][i]['t']))
+        for j in range(1, len(list1)):            
+                        
 
-                if '10:00' in str(j_obj2['timeseries'][i]['validTime']):
-                    dag21000 = str(j_obj2['timeseries'][i]['t'])
-                    print("Temp imorgon 10:00 " + str(j_obj2['timeseries'][i]['t']))
+
+            if list1[j] in str(j_obj2['timeseries'][i]['validTime']):
+                  list2[j] = str(j_obj2['timeseries'][i]['t'])
+                  print("Temp imorgon 08:00 " + str(j_obj2['timeseries'][i]['t']))
+                    
                 
-        
-
-       
-
         
 
 
